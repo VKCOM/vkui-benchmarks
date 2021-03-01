f62de10333649bf8eae00bd19341fbcecf86259e
Evgenii Khramkov <ewgeniux@gmail.com>
2021-03-01 23:31:42 +0300

    rich results

---
+++ scripts/format-results.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const manifests = JSON.parse(process.argv[2]);
+const links = JSON.parse(process.argv[3]);
+
+manifests.forEach((manifest) => {
+  console.log(`
+### ${manifest.url}
+#### full report:
+${links[manifest.url]}
+
+#### summary:
+
+| performance | accessibility | best-practices | seo | pwa |
+|-|-|-|-|-|
+| ${manifest.summary['performance']} | ${manifest.summary['accessibility']} | ${manifest.summary['best-practices']} | ${manifest.summary['seo']} | ${manifest.summary['pwa']} |
+`);
+})
