906cd68cb2687be0b9529e2d3b29bece0a6e42c4
Evgenii Khramkov <ewgeniux@gmail.com>
2021-03-02 17:22:13 +0300

    WIP

--- scripts/format-results-final.js
+++ scripts/format-results-final.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const vkuiReport = require(path.resolve(
   __dirname,
-  "../results/vkui/vkui.json"
+  "../results/results-vkui/vkui.json"
 ));
 
 function reportTable(app, run) {
