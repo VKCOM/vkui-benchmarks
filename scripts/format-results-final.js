9031a3af5abf9f62cb704fcb3f7effb2f00e12af
Evgenii Khramkov <ewgeniux@gmail.com>
2021-03-02 18:57:05 +0300

    add comparison labels

--- scripts/format-results-final.js
+++ scripts/format-results-final.js
@@ -8,10 +8,41 @@ const vkuiReport = require(path.resolve(
   "../results/results-vkui/vkui.json"
 ));
 
-function reportTable(app, run) {
-  const { type, url, reportUrl, summary } = run;
-  const { performance, accessibility, seo, pwa, bestPractices } = summary;
-  return `| ${app} | [${type}](${url}) | [report](${reportUrl}) | ${performance} | ${accessibility} | ${bestPractices} | ${seo} | ${pwa} |`;
+function compare(v1, v2) {
+  return v1 > v2 ? "🟢" : v1 < v2 ? "🔴" : "";
+}
+
+function reportCompare(app1, app1Run, app2, app2Run) {
+  const {
+    type: type1,
+    url: url1,
+    reportUrl: reportUrl1,
+    summary: summary1,
+  } = app1Run;
+  const {
+    performance: performance1,
+    accessibility: accessibility1,
+    seo: seo1,
+    pwa: pwa1,
+    bestPractices: bestPractices1,
+  } = summary1;
+
+  const {
+    type: type2,
+    url: url2,
+    reportUrl: reportUrl2,
+    summary: summary2,
+  } = app2Run;
+  const {
+    performance: performance2,
+    accessibility: accessibility2,
+    seo: seo2,
+    pwa: pwa2,
+    bestPractices: bestPractices2,
+  } = summary2;
+
+  return `| ${app1} | [${type1}](${url1}) | [report](${reportUrl1}) | ${performance1} ${compare(performance1, performance2)} | ${accessibility1} ${compare(accessibility1, accessibility2)} | ${bestPractices1} ${compare(bestPractices1, bestPractices2)} | ${seo1} ${compare(seo1, seo2)} | ${pwa1} ${compare(pwa1, pwa2)} |
+| ${app2} | [${type2}](${url2}) | [report](${reportUrl2}) | ${performance2} | ${accessibility2} | ${bestPractices2} | ${seo2} | ${pwa2} |`;
 }
 
 const SEPARATOR = `
@@ -36,7 +67,6 @@ fs.readdirSync(path.resolve(__dirname, "../results"))
 |-|-|-|-|-|-|-|-|`);
 
     ["default", "modals", "list"].forEach((type) => {
-      console.log(reportTable("vkui", vkuiReport.runs[type]));
-      console.log(reportTable(report.app, report.runs[type]));
+      console.log(reportCompare("vkui", vkuiReport.runs[type], report.app, report.runs[type]));
     });
   });
