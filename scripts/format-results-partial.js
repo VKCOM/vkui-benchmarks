1d9890e10c233929c30c441a0a46a80b2acf7cc1
Evgenii Khramkov <ewgeniux@gmail.com>
2021-03-02 12:12:39 +0300

    WIP

--- scripts/format-results-partial.js
+++ scripts/format-results-partial.js
@@ -5,12 +5,6 @@ const manifests = JSON.parse(process.argv[3]);
 const links = JSON.parse(process.argv[4]);
 
 manifests.forEach((manifest) => {
-//   if (i === 0 && !skipTitle) {
-//     console.log(`| app | url | report | performance | accessibility | best-practices | seo | pwa |
-// |-|-|-|-|-|-|-|-|`);
-//   }
-
   const urlTitle = manifest.url.includes("mode=modals") ? "modals" : manifest.url.includes("mode=burn") ? "list" : "default";
-
   console.log(`| ${app} | [${urlTitle}](${manifest.url}) | [full report](${links[manifest.url]}) | ${manifest.summary['performance']} | ${manifest.summary['accessibility']} | ${manifest.summary['best-practices']} | ${manifest.summary['seo']} | ${manifest.summary['pwa']} |`);
 });
