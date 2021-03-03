4c38ec0accf39c487874f19819805f36028f9442
gbowsky <grishaxtail@gmail.com>
2021-03-03 18:27:19 +0300

    Added "spectrum" to matrix

--- scripts/get-matrix.js
+++ scripts/get-matrix.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const output = process.argv[2]
-  ? { app: ["vkui", "vkui-versioned", "mui", "yandex", "f7"] }
-  : { app: ["vkui", "mui", "yandex", "f7"] };
+  ? { app: ["vkui", "vkui-versioned", "mui", "yandex", "f7", "spectrum"] }
+  : { app: ["vkui", "mui", "yandex", "f7", "spectrum"] };
 
 console.log(output);
