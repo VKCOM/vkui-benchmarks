dbfc60518bf75055d20ad7261135cea301da4a6c
Evgenii Khramkov <ewgeniux@gmail.com>
2021-03-04 15:24:28 +0300

    Fix

--- scripts/get-matrix.js
+++ scripts/get-matrix.js
@@ -4,4 +4,4 @@ const output = process.argv[2]
   ? { app: ["vkui", "vkui-versioned", "mui", "yandex", "f7", "spectrum"] }
   : { app: ["vkui", "mui", "yandex", "f7", "spectrum"] };
 
-console.log(output);
+console.log(JSON.stringify(output));
