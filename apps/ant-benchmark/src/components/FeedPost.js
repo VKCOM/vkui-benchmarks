434cf26f11c179bcdeb591d1e47216f5c38a9b49
gbowsky <grishaxtail@gmail.com>
2021-03-19 12:24:10 +0300

    fix(ant): little newsfeed improvements

--- apps/ant-benchmark/src/components/FeedPost.js
+++ apps/ant-benchmark/src/components/FeedPost.js
@@ -1,7 +1,7 @@
 import {Avatar, Card, Col} from "antd";
 
 export const FeedPost = ({avatar, children, time, authorName}) => {
-    return <Col span={6}>
+    return <Col span={12}>
         <Card>
             <Card.Meta className="FeedPost__meta" avatar={<Avatar src={avatar}/>} title={authorName}
                        description={time}/>
